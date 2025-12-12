ebcdc3d792c86aaaf144d1389745bbdbe363e9bc
uttam24uttam <uttamhamsaraj24@gmail.com>
2025-12-12 07:55:58 +0530

    updating test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,15 +1,14 @@
-jest.config.js: |
-    module.exports = {
+module.exports = {
     testEnvironment: 'node',
-        coverageDirectory: 'coverage',
-            testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
-                collectCoverageFrom: ['**/*.js', '!**/node_modules/**', '!**/coverage/**'],
-                    coverageThreshold: {
+    coverageDirectory: 'coverage',
+    testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+    collectCoverageFrom: ['**/*.js', '!**/node_modules/**', '!**/coverage/**'],
+    coverageThreshold: {
         global: {
             branches: 50,
-                functions: 50,
-                    lines: 50,
-                        statements: 50,
-      },
+            functions: 50,
+            lines: 50,
+            statements: 50,
+        },
     },
 };
